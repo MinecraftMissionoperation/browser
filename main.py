c0c8760a1e3b8e0ede42be6c58586f599199bc7e
MinecraftMissionoperation <S040399@students.lmsd.org>
2025-09-05 12:04:52 -0400

    Update and rename main.py to main.html

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-import sys
-
-app = QApplication(sys.argv)
-browser = QWebEngineView()
-browser.load("https://google.com")
-browser.show()
-sys.exit(app.exec_())
